aef093d5110009c5ed0360d3528c3919ebc9d9db
liangxifeng833 <liangxifeng833@163.com>
2014-11-11 22:37:43 +0800

    controller study

---
+++ php/yii2/basic/controllers/dir1/PostCommentController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\controllers\dir1;
+use Yii;
+use yii\web\Controller;
+
+class PostCommentController extends Controller
+{
+    public function actionIndex()
+    {
+        echo 'this is a test controller!';
+    }
+}
