f05bf5cbb6330ef45941723e0c84c2a7660323c2
liangxifeng833 <liangxifeng833@163.com>
2014-11-23 22:19:31 +0800

    update dir1

--- php/yii2/basic/controllers/dir1/PostCommentController.php
+++ php/yii2/basic/controllers/dir1/PostCommentController.php
@@ -7,14 +7,22 @@ class PostCommentController extends Controller
 {
     public function actionIndex()
     {
+         //重定向
+         //return $this->redirect('/index.php?r=site/index');
          //察看数据库连接
          $db = \Yii::$app->db;
-         var_dump($db);
-         echo '<br>';
+         //var_dump($db);
          //查看缓存
          $cache = \Yii::$app->cache;
-         var_dump($cache);exit;
+         //var_dump($cache);exit;
          echo '<br>this is a test controller!';
+         $name = $this->getName();
+         var_dump($name);exit;
 
+    }
+    //私有方法必须在类内部调用，不能在路由规则内访问
+    private function getName()
+    {
+        return 'my name is liangxifeng';
     }
 }
