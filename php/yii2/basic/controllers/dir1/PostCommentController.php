6cc63b523293b1496a57dc3a6ccc50fb45589cc4
liangxifeng833 <liangxifeng833@163.com>
2014-11-24 10:33:26 +0800

    dir1 update

--- php/yii2/basic/controllers/dir1/PostCommentController.php
+++ php/yii2/basic/controllers/dir1/PostCommentController.php
@@ -5,20 +5,34 @@ use yii\web\Controller;
 
 class PostCommentController extends Controller
 {
-    public function actionIndex()
-    {
-         //重定向
-         //return $this->redirect('/index.php?r=site/index');
-         //察看数据库连接
-         $db = \Yii::$app->db;
-         //var_dump($db);
-         //查看缓存
-         $cache = \Yii::$app->cache;
-         //var_dump($cache);exit;
-         echo '<br>this is a test controller!';
-         $name = $this->getName();
-         var_dump($name);exit;
+    //修改默认控制器
+    public $defaultAction = 'param';
 
+    public function actionIndex($id,$version=null)
+    {
+
+        //重定向
+        //return $this->redirect('/index.php?r=site/index');
+        //察看数据库连接
+        $db = \Yii::$app->db;
+        //var_dump($db);
+        //查看缓存
+        $cache = \Yii::$app->cache;
+        //var_dump($cache);exit;
+        echo '<br>this is a test controller!';
+        $name = $this->getName();
+        var_dump($name);exit;
+
+    }
+    //action参数以字符串的形式
+    public function actionParam($id,$version=null)
+    {
+        var_dump($id);exit;
+    }
+    //action参数以数组的形式
+    public function actionParamArray(array $id,$version=null)
+    {
+        var_dump($id);exit;
     }
     //私有方法必须在类内部调用，不能在路由规则内访问
     private function getName()
