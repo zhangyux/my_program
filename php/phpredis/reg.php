0e99408fe717647bf638bdb765230d9995852030
liangxifeng833 <liangxifeng833@163.com>
2014-12-21 22:27:06 +0800

    redis list set login

--- php/phpredis/reg.php
+++ php/phpredis/reg.php
@@ -8,6 +8,12 @@ $age = $_POST['age'];
 $uid = $redis->incr("userid");
 //批量增加/修改hash table
 $res = $redis->hmset("user:".$uid,array("username"=>$username,"password"=>$password,"age"=>$age,"uid"=>$uid));
+
+//插入队列uid,用来做分页
+$redis->rpush("uid",$uid);
+
+//用户注册的时候容易找到该用户，否则找不到
+$redis->set("username:".$username,$uid);
 //删除用 $redis->del("user:".$uid);
 if($res)
 {
