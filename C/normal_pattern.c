ca93bc5d77929ea36931cbcaac92c81f15b75728
liangxifeng833 <liangxifeng833@163.com>
2015-05-19 20:49:09 +0800

    update normal_pattern.c

--- C/normal_pattern.c
+++ C/normal_pattern.c
@@ -1,5 +1,6 @@
 /*
  * C语言实现朴素模式匹配算法
+ * 在主字串s中查找字串t，如果匹配成功则返回t在s中出现的位置
  * date:2015-05-18
  */
 #include<stdio.h>
@@ -7,8 +8,8 @@ void main(int argc, char *argv[])
 {
     char s[] = "abcabcdefg"; 
     char t[] = "abcdefi"; 
-    int sLen = sizeof(s)-1; 
-    int tLen = sizeof(t)-1; 
+    int sLen = sizeof(s)-1;  //s的长度
+    int tLen = sizeof(t)-1;  //t的长度
     int i=0, j=0; 
     while(i<sLen  && j<tLen)
     {
