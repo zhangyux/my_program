d1763a7cb96e084eb6f8152e5702b1e6e1eb8761
liangxifeng833 <liangxifeng833@163.com>
2016-03-04 09:29:20 +0800

    add java test project

---
+++ java/testProject/src/com/lxf/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lxf;
+
+public class HelloWorld {
+	public static void main(String[] args){
+		//变量定义测试
+		String love = "liangxifeng";
+		love = love + " too";
+		System.out.println("变量love的内容是: "+love);
+	}
+}
