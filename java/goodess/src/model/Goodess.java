4a86cf7b00424417dca6d7bca772601dd69b6d03
liangxifeng833 <liangxifeng833@163.com>
2016-04-21 09:47:29 +0800

    add goodess

---
+++ java/goodess/src/model/Goodess.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.util.Date;
+
+public class Goodess {
+	private Integer id;
+	private String user_name;
+	private Integer sex;
+	private Date birthday;
+	private String email;
+	private Date add_time;
+	private Date update_time;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public Integer getSex() {
+		return sex;
+	}
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getAdd_time() {
+		return add_time;
+	}
+	public void setAdd_time(Date add_time) {
+		this.add_time = add_time;
+	}
+	public Date getUpdate_time() {
+		return update_time;
+	}
+	public void setUpdate_time(Date update_time) {
+		this.update_time = update_time;
+	}
+	
+}
