8c6382a736d148863efd64fc9fe5f29862423d99
lxf <lxf@lxf-desktop.(none)>
2016-05-10 22:43:34 +0800

    0510

--- java/goodess/src/action/GoodessAction.java
+++ java/goodess/src/action/GoodessAction.java
@@ -12,6 +12,38 @@ import dao.GoodessDao;
 import model.Goodess;
 
 public class GoodessAction {
+	
+	//新增女神方法
+	public void add(Goodess goodess) throws Exception
+	{
+		GoodessDao dao = new GoodessDao();
+		dao.addGoddess(goodess);
+	}
+	//修改女神方法
+	public void edit(Goodess goodess) throws Exception
+	{
+		GoodessDao dao = new GoodessDao();
+		dao.updateGoddess(goodess);
+	}
+	//删除女神方法
+	public void del(Integer id) throws Exception
+	{
+		GoodessDao dao = new GoodessDao();
+		dao.delGoddess(id);
+	}
+	//根据条件查询女神
+	public List<Goodess> query(List<Map<String,Object>> params) throws Exception
+	{
+		GoodessDao dao = new GoodessDao();
+		return dao.query(params);
+	}
+	//查询单个女神
+	public Goodess get(Integer id) throws SQLException
+	{
+		GoodessDao dao = new GoodessDao();
+		return dao.get(id);
+	}
+	
 
 	/**
 	 * @param args
@@ -22,6 +54,7 @@ public class GoodessAction {
 		GoodessDao g = new GoodessDao();
 		//查询女神多条记录
 		//定义查询参数
+
 		List<Map<String,Object>> params = new ArrayList<Map<String,Object>>();
 		Map<String,Object> param = new HashMap<String,Object>();
 		param.put("name", "user_name");
@@ -35,19 +68,22 @@ public class GoodessAction {
 		params.add(map);
 		List<Goodess> gs = g.query(params);
 		//遍历查询的结果
+		
 		for (Goodess goodess : gs) {
 			System.out.println(goodess.toString());		
 		}
+		
 			
-		/*
+		
 		Goodess gs1 = new Goodess();
 		gs1.setUser_name("liangxifeng modify new");
 		gs1.setSex(1);
 		gs1.setBirthday(new Date());
 		gs1.setUpdate_time(new Date());
-		gs1.setId(1);
+		//gs1.setId(1);
 		//新增女神
-		//g.addGoddess(gs1);
+		g.addGoddess(gs1);
+		/*
 		//修改女神
 		g.updateGoddess(gs1);
 		//删除女神
