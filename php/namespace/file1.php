68a323f399d485d10fc24127c25841864327c014
liangxifeng833 <liangxifeng833@163.com>
2016-08-13 10:44:54 +0800

    add php namespace

---
+++ php/namespace/file1.php
@@ -0,0 +1,17 @@
+<?php
+namespace Foo\Bar\subnamespace;
+
+const FOO = 1;
+function foo() {
+
+        echo "hello this is file1 foo\n";
+}
+class foo
+{
+    static function staticmethod() 
+    {
+        echo "hello this is file1 staticmethod\n";
+    }
+
+}
+?>
