4df987757df9613cceb327f62660a25d7b503c67
liangxifeng833 <liangxifeng833@163.com>
2019-04-20 15:19:12 +0800

    add static

---
+++ java/es-project/src/main/java/com/soufang/esproject/web/controller/admin/AdminController.java
@@ -0,0 +1,22 @@
+package com.soufang.esproject.web.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Description: es-project
+ * Create by liangxifeng on 19-4-15
+ */
+@Controller
+public class AdminController {
+    @GetMapping("/admin/center")
+    public String adminCenterPage(){
+        return "admin/center";
+    }
+
+    @GetMapping("/admin/welcome")
+    public String welcomePage(){
+        return "admin/welcome";
+
+    }
+}
