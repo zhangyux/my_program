2fb62de7d636a8010d5a5455f56b10994868add1
liangxifeng833 <liangxifeng833@163.com>
2019-04-29 18:27:06 +0800

    upload img

--- java/es-project/src/main/java/com/soufang/esproject/web/controller/admin/AdminController.java
+++ java/es-project/src/main/java/com/soufang/esproject/web/controller/admin/AdminController.java
@@ -1,7 +1,24 @@
 package com.soufang.esproject.web.controller.admin;
 
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import com.soufang.esproject.base.ApiResponse;
+import com.soufang.esproject.service.house.IQiNiuService;
+import com.soufang.esproject.web.dto.QiNiuPutRet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Description: es-project
@@ -9,6 +26,11 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class AdminController {
+    @Autowired
+    IQiNiuService iQiNiuService;
+    @Autowired
+    Gson gson;
+
     @GetMapping("/admin/center")
     public String adminCenterPage(){
         return "admin/center";
@@ -23,5 +45,72 @@ public class AdminController {
     @GetMapping("/admin/login")
     public String adminLoginPage(){
         return "admin/login";
+    }
+
+    @GetMapping("/admin/add/house")
+    public String addHousePage(){
+        return "admin/house-add";
+    }
+
+    @GetMapping("/admin/house/list")
+    public String houseListPage(){
+        return "admin/house-list";
+    }
+
+    /**
+     * 上传图片到本地
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "/admin/upload/locphoto", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadLocPhoto(@RequestParam("file") MultipartFile file ){
+        if( file.isEmpty() ){
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+        String fileName = file.getOriginalFilename();
+        File target = new File("/home/lxf/git/user_liangxifeng833/my_program/java/es-project/tmp/" + fileName);
+        try {
+            file.transferTo(target);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+        return ApiResponse.ofSuccess(null);
+    }
+
+    /**
+     * 上传图片到七牛云
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "/admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file") MultipartFile file ){
+        if( file.isEmpty() ){
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+        try {
+            //获取内容中的文件流
+            InputStream fileInputStream = file.getInputStream();
+            //将文件传输到远程七牛云
+            Response response = iQiNiuService.uploadFile(fileInputStream);
+            if( response.isOK() ){
+                QiNiuPutRet qiNiuPutRet = gson.fromJson(response.bodyString(), QiNiuPutRet.class);
+                return ApiResponse.ofSuccess(qiNiuPutRet);
+            }else{
+                return ApiResponse.ofMessage(response.statusCode,response.getInfo());
+            }
+        } catch (QiniuException e){
+            Response response = e.response;
+            try {
+                return ApiResponse.ofMessage(response.statusCode,response.bodyString());
+            } catch (QiniuException e1) {
+                e1.printStackTrace();
+                return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+            }
+        } catch (IOException e) {
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
     }
 }
